5335ce6c327f3d2f0742e074a0347e8ff1bc1e8a
Carson169 <carsonjsamples@icloud.com>
2026-09-04 14:24:02 -0400

    Completed Week 1 Lab 1

--- BoxOffice.java
+++ BoxOffice.java
@@ -1,20 +1,28 @@
+//Carson Samples
+//Lab Assignment 1
+//9.4.26
+//Course CMP-129-80231
 import java.util.*;
 public class BoxOffice 
 {
     public static void main(String[] args) 
     {
-            Scanner FunnyScanner = new Scanner(System.in);
-            System.out.println("Whats the name of the movie: ");
-            String movie = FunnyScanner.nextLine();
-            System.out.println("How many adult tickets were sold: ");
-            Double adult = FunnyScanner.nextDouble();
-            System.out.println("How many child tickets were sold: ");
-            Double child = FunnyScanner.nextDouble();
-            System.out.println("Movie Name:                                   " + movie);
-            System.out.println("Adult Tickets Sold:                           " + adult);
-            System.out.println("Child Tickets Sold:                           " + child);
-            System.out.println("Gross Box Office Profit:                    $ " + ((adult * 10) + (child * 6)));
-            System.out.println("Net Box Office Profit:                      $ " + (((adult * 10) + (child * 6)) * 0.20));
-            System.out.println("Amount Paid to Distributor:                 $ " + (((adult * 10) + (child * 6)) * 0.80));
+            Scanner FunnyScanner = new Scanner(System.in); //initalizing scanner
+            System.out.println("Whats the name of the movie: "); //prompting user for name of movie
+            String movie = FunnyScanner.nextLine(); //setting movie variable to the name of the movie
+            System.out.println("How many adult tickets were sold: "); //prompting user for amount of adult tickets sold
+            Double adult = FunnyScanner.nextDouble(); //setting adult variable to the amount of adult tickets sold
+            System.out.println("How many child tickets were sold: "); //prompting user for amount of child tickets
+            Double child = FunnyScanner.nextDouble(); //setting child variable to the amount of child tickets
+            Double gross = ((adult * 10) + (child * 6)); //calculating gross box office profit
+            Double box = (((adult * 10) + (child * 6)) * 0.20); //calculating net box office profit
+            Double distrib = (((adult * 10) + (child * 6)) * 0.80); //calculating amount paid to distributor
+            System.out.println("Movie Name: " + movie); //displaying movie name
+            System.out.printf("Adult Tickets Sold: %.0f\n" , adult); //displaying adult tickets sold
+            System.out.printf("Child Tickets Sold: %.0f\n" , child); //displaying child tickets sold
+            System.out.printf("Gross Box Office Profit: $%.2f\n" , gross); //calculating gross box office profit
+            System.out.printf("Net Box Office Profit: $%.2f\n" , box); //calculating net box office profit
+            System.out.printf("Amount Paid to Distributor: $%.2f\n" , distrib); //calculating amount paid to distributor
+            FunnyScanner.close(); //closing scanner to prevent memories leak
     }    
 }
--- InterestEarned.java
+++ InterestEarned.java
@@ -1,3 +1,26 @@
-public class InterestEarned {
-    
+//Carson Samples
+//Lab Assignment 1
+//9.4.26
+//Course CMP-129-80231
+import java.util.*;
+public class InterestEarned 
+{
+    public static void main(String[] args) 
+    {
+        Scanner FunnyScanner2 = new Scanner(System.in); //initializing scanner
+        System.out.println("What is the principal amount: "); //asking for principal amount
+        Double prince = FunnyScanner2.nextDouble(); //setting variable to principal amount input
+        System.out.println("What is the annual interest rate: "); //asking for annual interest rate
+        Double rate = FunnyScanner2.nextDouble(); //setting variable to annual interest rate input
+        System.out.println("How many times is the interest compounded per year: "); //asking for compound amount
+        Double year = FunnyScanner2.nextDouble(); //setting variable to compound amount
+        Double finalsavings = prince * Math.pow((1 + rate / year), year); //calculating the final savings amount
+        Double earned = finalsavings - prince; //calculating the total interest earned
+        System.out.println("Annual Interest Rate: " + rate*100 +"%"); //displaying interest rate
+        System.out.printf("Times Compounded: %.0f\n" , year); //displaying compound amount
+        System.out.printf("Principal: $%.2f\n" , prince); //displaying principal
+        System.out.printf("Interest Earned: $%.2f\n" , earned); //displaying interest earned
+        System.out.printf("Amount in Savings: $%.2f\n" , finalsavings); //displaying amount in savings
+        FunnyScanner2.close(); //closing scanner to prevent memory leak
+    }
 }
